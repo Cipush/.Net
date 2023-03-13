1e44a6d132d40704d2b116f6e30bcf7e1e88cecc
Meri <cipush7@gmail.com>
2023-03-13 16:12:16 +0100

    task2

---
+++ Class 03/StudentGroup/Program.cs
@@ -0,0 +1,35 @@
+﻿// Task 2
+
+
+string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Ana", "Maja", "Ivan", "Marko", "Igor" };
+
+Console.WriteLine("Enter student group (1 or 2):");
+int groupNumber = int.Parse(Console.ReadLine());
+
+if (groupNumber == 1)
+{
+    Console.WriteLine("The students in G1 are:");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (groupNumber == 2)
+{
+    Console.WriteLine("The students in G2 are:");
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid group number.");
+}
+
+Console.ReadLine(); 
+        
+    
+
+
---
+++ Class 03/SumOfEven/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 1
+
+
+int[] numbers = { 4, 3, 7, 3, 2, 8 };
+int sumOfEven = 0;
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sumOfEven += number;
+    }
+}
+
+Console.WriteLine("The result is: " + sumOfEven);
+Console.ReadLine();
